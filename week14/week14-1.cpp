6b10b54c29fd26e51c72f8e429bf7e480de98e8f
2486ISYOU <144758307+2486ISYOU@users.noreply.github.com>
2024-12-12 16:18:14 +0800

    week14

---
+++ week14/week14-1.cpp
@@ -0,0 +1,16 @@
+//week14-1.cpp¥¼§¹¦¨
+//leetcode 2.
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode*ans= new ListNode();
+        ListNode*now=ans;
+        while(l1!=nullptr){
+            now->next =new ListNode(l1->val);
+            now=now->next;
+            l1=l1->next;
+        }
+        return ans->next;
+    }
+};
